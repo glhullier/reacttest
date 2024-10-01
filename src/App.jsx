75fbced61d9dbef1bd284beb951d05b3841011ef
glhullier <g.lhullier@it-students.fr>
2024-10-01 05:33:04 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1 className="title">Bonjour les gens</h1>;
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque delectus
+        error expedita eum deserunt iure dolores consequuntur, necessitatibus
+        iusto officia placeat excepturi a neque quae voluptatum repellat eius
+        fuga laborum.
+      </p>
+    </div> //Usage du html avec ClassName et div
+  );
+}
+
+export default App;
